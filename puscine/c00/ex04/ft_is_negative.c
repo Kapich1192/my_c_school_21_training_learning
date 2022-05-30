838dda3f41a576002f05189e0278b489e94d6b18
kapich1192 <kapich1192@yandex.ru>
2022-05-30 17:16:31 +0300

    ex04 OK

---
+++ puscine/c00/ex04/ft_is_negative.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Dmitriy <kapich1192@yandex.ru>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 17:04:16 by Dmitriy           #+#    #+#             */
+/*   Updated: 2022/05/30 17:14:17 by Dmitriy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_is_negative(int n)
+{
+	if (n >= 0)
+	{
+		ft_putchar('P');
+	}
+	else
+	{
+		ft_putchar('N');
+	}
+}
+
+int	main(void)
+{
+	ft_is_negative(-9);
+	ft_is_negative(8);
+	return (0);
+}
