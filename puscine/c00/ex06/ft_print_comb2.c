ebf75373741bec6ef828a8e6053982a5516d4260
kapich1192 <kapich1192@yandex.ru>
2022-05-31 12:43:45 +0300

    ex06 OK

---
+++ puscine/c00/ex06/ft_print_comb2.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Dmitriy <kapich1192@yandex.ru>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/31 11:44:30 by Dmitriy           #+#    #+#             */
+/*   Updated: 2022/05/31 12:41:00 by Dmitriy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	print(char a, char b, char c, char d)
+{
+	ft_putchar(a);
+	ft_putchar(b);
+	ft_putchar(' ');
+	ft_putchar(c);
+	ft_putchar(d);
+	if (a == '9' && b == '8' && c == '9' && d == '9')
+	{
+		ft_putchar(' ');
+	}
+	else
+	{
+		ft_putchar(',');
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	char	a;
+	char	b;
+	char	c;
+	char	d;
+
+	a = '0';
+	while (a <= '9')
+	{
+		b = '0';
+		while (b <= '9')
+		{
+			c = '0';
+			while (c <= '9')
+			{
+				d = '0';
+				while (d <= '9')
+				{
+					print(a, b, c, d);
+					d++;
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb2();
+	return (0);
+}
