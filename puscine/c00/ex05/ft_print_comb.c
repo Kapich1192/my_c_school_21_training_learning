2d8263f862a86a79b529b2d6ced8ac1b0d3e0154
kapich1192 <kapich1192@yandex.ru>
2022-05-31 11:34:10 +0300

    ex05 OK

--- puscine/c00/ex05/ft_print_comb.c
+++ puscine/c00/ex05/ft_print_comb.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: Dmitriy <kapich1192@yandex.ru>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/30 17:30:08 by Dmitriy           #+#    #+#             */
-/*   Updated: 2022/05/30 21:11:12 by Dmitriy          ###   ########.fr       */
+/*   Created: 2022/05/31 09:46:43 by Dmitriy           #+#    #+#             */
+/*   Updated: 2022/05/31 11:23:14 by Dmitriy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_next(char n1, char n2, char n3)
+void	print_next(char n1, char n2, char n3)
 {
 	ft_putchar(n1);
 	ft_putchar(n2);
@@ -29,61 +29,60 @@ void	ft_next(char n1, char n2, char n3)
 int	factorial(int n)
 {
 	int	temp;
+	int	i;
 
 	temp = 1;
-	while (n > 0)
+	i = 1;
+	while (i <= n)
 	{
-		temp *= n;
-		n--;
+		temp *= i;
+		i++;
 	}
-	return temp;
+	return (temp);
 }
 
-int	set_size(int count_points, int variants)
+int	count_variants(int c, int v)
 {
-	int	size_set;
+	int	set_size;
 
-	size_set = factorial(count_points) /
-		(factorial(count_points - variants) * factorial(variants));
-
-	return size_set;
+	set_size = factorial(c) / (factorial(c - v) * factorial(v));
+	return (set_size);
 }
 
-void ft_print_comb(void)
+void	ft_print_comb(void)
 {
-	char	n1, n2, n3;
+	char	n1;
+	char	n2;
+	char	n3;
 	int	i;
-	
+
 	n1 = '0';
 	n2 = '1';
 	n3 = '2';
 	i = 0;
-	while( i < 790)
+	while (i < count_variants(10, 3))
 	{
-		if(n1 < n2 && n2 < n3)
+		if (n1 < n2 && n2 < n3)
 		{
-			ft_next(n1, n2, n3);
+			print_next(n1, n2, n3);
+			i++;
 		}
-
-		if(n3 == '9')
+		if (n3 == '9')
 		{
 			n3 = '0';
 			n2++;
 		}
-
 		if (n2 == '9')
 		{
 			n2 = '0';
 			n1++;
 		}
-
 		n3++;
-		i++;
 	}
 }
 
 int	main(void)
 {
 	ft_print_comb();
-	return(0);
+	return (0);
 }
