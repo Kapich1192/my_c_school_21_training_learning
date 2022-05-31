5ae5ff920de6070cdd96d2c6b5efc10ca2f1e3f5
kapich1192 <kapich1192@yandex.ru>
2022-05-31 13:12:14 +0300

    ex07 OK

---
+++ puscine/c00/ex07/ft_putnbr.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Dmitriy <kapich1192@yandex.ru>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/31 12:48:49 by Dmitriy           #+#    #+#             */
+/*   Updated: 2022/05/31 13:08:11 by Dmitriy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	unsigned int	n;
+
+	n = nb;
+	if (nb < 0)
+	{
+		n = nb * -1;
+		ft_putchar('-');
+	}
+	if(n >= 10)
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+	else
+		ft_putchar(n + '0');
+}
+
+int	main(void)
+{
+	ft_putnbr(100);
+	return (0);
+}
