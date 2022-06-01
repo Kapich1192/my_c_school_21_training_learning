1a55634c546f36a6f38555a46a9f22e981d1e1c0
kapich1192 <kapich1192@yandex.ru>
2022-06-01 15:30:32 +0300

    ex04 n OK

---
+++ puscine/c01/ex04/ft_putstr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Dmitriy <kapich1192@yandex.ru>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/01 15:21:19 by Dmitriy           #+#    #+#             */
+/*   Updated: 2022/06/01 15:30:00 by Dmitriy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(void)
+{
+	ft_putstr("Hello World!");
+	return (0);
+}
